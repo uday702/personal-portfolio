14419ac4cb443bd3faf11348f73ee1d86ca9f68e
uday702 <84147477+uday702@users.noreply.github.com>
2021-06-28 05:35:48 +0530

    Helping hands project
    Helping hands website will tracks the money that how much amount we
    Have spent on student
    Its used for solving the students problems on money
    Helping others can help u live longer
    Helping others makes us happy
    Helping others promotes positive behaviour in teens

---
+++ details_entry3.php
@@ -0,0 +1,61 @@
+<?php
+//this is phpmyadmin connection and do you give parent details and see in this database
+$server_name="localhost";
+$username="root";
+$password="";
+$database_name="database 123";
+$conn=mysqli_connect($server_name
+,$username, $password, $database_na
+me);
+if(!$conn)
+{
+11
+die("Connection Failed: "
+mysqli_connect_error());
+.
+}
+if(isset($_POST['save']))
+{
+$enter_name
+=
+$_POST['enter_name'];
+$village
+=
+$_POST['village'];
+$email
+=
+$_POST['email'];
+$phone_number
+=
+$_POST['phone_nunber'];
+$state
+=
+$_POST['state'];
+$fund_amount
+=
+$_POST['fund_amount'];
+$gender =
+$_POST['gender'];
+$sql_query
+"INSERT
+INTO entry_details
+(enter_name, village, email, phone_number, state, fund_amount, gender)
+VALUES
+('$enter_name', '$village','$email', '$phone_number','$state','$fund_amount','$gender')";
+if (mysqli_query($conn,
+$sql_query))
+{
+echo "New Details
+Entry inserted successfully !";
+}
+else
+{
+IT
+echo "Error:
+11 11
+$sql .
+. mysqli_error($conn);
+}
+mysqli_close($conn);
+}
+?>
